3589f2c0bf893ab015463672fe912c97cc153f1e
Elisabeth Diana Kartika S <elisabeth.dianaks@gmail.com>
2017-11-28 11:33:33 +0700

    Initial commit

---
+++ WebApplication2/Controllers/InputPemasukanController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class InputPemasukanController : Controller
+    {
+        // GET: InputPemasukan
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(InputPemasukan inputPemasukan)
+        {
+            int pemasukan = inputPemasukan.PemasukanPerBulan;
+
+            return View();
+        }
+    }
+}
